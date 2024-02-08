d58505b1b83fb3d32b16696adc6950b08f4c12fe
Plsr <christian@gigs.com>
2024-02-08 15:31:41 +0100

    Move fluff out in components to make main page easier to read

--- app/(manage)/backoffice/page.tsx
+++ app/(manage)/backoffice/page.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Phone, ShoppingCart, Smartphone, User2 } from "lucide-react";
-import Image from "next/image";
 import { getSubscriptionsByUser } from "@/lib/api";
 import { ManagePlanCard } from "@/components/ManagePlanCard";
+import { SideNav } from "@/components/SideNav";
 
 export default async function BackOfficePage() {
   const subscriptions = await getSubscriptionsByUser(
@@ -13,59 +11,11 @@ export default async function BackOfficePage() {
   return (
     <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-white lg:block dark:bg-gray-800/40">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-[60px] items-center border-b px-6">
-            <Image
-              alt="Company Logo"
-              className="h-6 w-auto"
-              height={20}
-              src="/tigr-logo.webp"
-              width={100}
-            />
-          </div>
-          <div className="flex-1 overflow-auto py-2">
-            <nav className="grid items-start px-4 text-sm font-medium">
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="#"
-              >
-                <ShoppingCart className="h-4 w-4" />
-                Orders
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="#"
-              >
-                <User2 className="h-4 w-4" />
-                Profile
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="#"
-              >
-                <Smartphone className="h-4 w-4" />
-                Devices
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg bg-gray-50 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
-                href="#"
-              >
-                <Phone className="h-4 w-4" />
-                Phone Plans
-              </Link>
-            </nav>
-          </div>
-        </div>
+        <SideNav />
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-white px-6 dark:bg-gray-800/40"></header>
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 bg-gray-50">
-          <div className="flex items-center">
-            <h1 className="font-semibold text-lg md:text-2xl">Phone Plans</h1>
-            <Button className="ml-auto" size="sm">
-              Add Plan
-            </Button>
-          </div>
+          <Header />
           <div className="grid grid-cols-2 gap-6">
             {subscriptions.map((subscription) => (
               <ManagePlanCard
@@ -79,3 +29,14 @@ export default async function BackOfficePage() {
     </div>
   );
 }
+
+const Header = () => {
+  return (
+    <div className="flex items-center">
+      <h1 className="font-semibold text-lg md:text-2xl">Phone Plans</h1>
+      <Button className="ml-auto" size="sm">
+        Add Plan
+      </Button>
+    </div>
+  );
+};
---
+++ components/SideNav.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Phone, ShoppingCart, Smartphone, User2 } from "lucide-react";
+
+export const SideNav = () => {
+  return (
+    <div className="flex h-full max-h-screen flex-col gap-2">
+      <div className="flex h-[60px] items-center border-b px-6">
+        <Image
+          alt="Company Logo"
+          className="h-6 w-auto"
+          height={20}
+          src="/tigr-logo.webp"
+          width={100}
+        />
+      </div>
+      <div className="flex-1 overflow-auto py-2">
+        <nav className="grid items-start px-4  gap-2 text-sm font-medium">
+          <Link
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+            href="#"
+          >
+            <ShoppingCart className="h-4 w-4" />
+            Orders
+          </Link>
+          <Link
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+            href="#"
+          >
+            <User2 className="h-4 w-4" />
+            Profile
+          </Link>
+          <Link
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+            href="#"
+          >
+            <Smartphone className="h-4 w-4" />
+            Devices
+          </Link>
+          <Link
+            className="flex items-center gap-3 rounded-lg bg-gray-50 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
+            href="#"
+          >
+            <Phone className="h-4 w-4" />
+            Phone Plans
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+};
